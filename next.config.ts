516a1006058248b0ac4534bb38d7d5ada95ec965
pianist22 <priyanshusaha944@gmail.com>
2026-04-08 20:41:28 +0530

    Added the configuration for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // IMPORTANT
-  images: {
-    unoptimized: true, // required for GitHub Pages
-  },
-  basePath: "/GoAffpro-documentation",
-  assetPrefix: "/GoAffpro-documentation",
+  // nothing needed for vercel
 };
 
 export default nextConfig;
